c9759ac919e7457c89a382234d23e97563952c76
Justin Moon <mail@justinmoon.com>
2019-05-26 15:09:42 -0500

    use a queue instead of channel for inbox

--- wire.go
+++ wire.go
@@ -9,27 +9,19 @@ import (
 	"github.com/btcsuite/btcd/connmgr"
 	"github.com/btcsuite/btcd/peer"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/sheerun/queue"
 )
 
-//type Failure string
-
 // globals
-var getaddrSent = make(chan struct{}) // TODO: delete
 
 // q contains addresses we need to visit
-var inbox = make(chan *wire.NetAddress)
+//var inbox = make(chan *wire.NetAddress)
+var inbox = queue.New()
 var online = make([]*peer.Peer, 2)
 var offline = make([]*peer.Peer, 2)
 
 // HandleVerack prints when we've received a verack message
-func HandleVerack(p *peer.Peer, msg *wire.MsgVerAck) {
-	getaddr := wire.NewMsgGetAddr()
-	p.QueueMessage(getaddr, getaddrSent)
-	<-getaddrSent
-	online = append(online, p)
-}
 
-// HandleAddr prints when we've received a verack message
 // queryDNS loads some initial wire.NetAddress instances
 // into the inbox channel
 func queryDNS() {
@@ -38,13 +30,17 @@ func queryDNS() {
 	connmgr.SeedFromDNS(&params, defaultRequiredServices,
 		net.LookupIP, func(addrs []*wire.NetAddress) {
 			for _, addr := range addrs {
-				inbox <- addr
+				inbox.Append(NAToS(addr))
 			}
 		})
 	<-time.After(time.Second * 10) // HACK!!!
 }
 
-func handshake(addr wire.NetAddress) {
+func NAToS(na *wire.NetAddress) string {
+	return fmt.Sprintf("[%s]:%d", na.IP, na.Port)
+}
+
+func visit(a string) {
 	var finished = make(chan bool)
 	peerCfg := &peer.Config{
 		UserAgentName:    "mooniversity",
@@ -53,33 +49,37 @@ func handshake(addr wire.NetAddress) {
 		Services:         0,
 		TrickleInterval:  time.Second * 10,
 		Listeners: peer.MessageListeners{
-			OnVerAck: HandleVerack,
+			OnVerAck: func(p *peer.Peer, msg *wire.MsgVerAck) {
+				getaddr := wire.NewMsgGetAddr()
+				getaddrSent := make(chan struct{}) // TODO: delete
+				p.QueueMessage(getaddr, getaddrSent)
+				<-getaddrSent
+				online = append(online, p)
+			},
 			OnAddr: func(p *peer.Peer, msg *wire.MsgAddr) {
 				if len(msg.AddrList) > 1 {
-					fmt.Printf("received %d addr from %s\n", len(msg.AddrList), addr.IP)
+					fmt.Printf("received %d addr from %s\n", len(msg.AddrList), a)
 					for _, addr := range msg.AddrList {
-						inbox <- addr
+						inbox.Append(NAToS(addr))
 					}
 				}
 				finished <- true
 			},
 		},
 	}
-	// hack to format ipv6 strings
-	addrString := fmt.Sprintf("[%s]:%d", addr.IP, addr.Port)
-	p, err := peer.NewOutboundPeer(peerCfg, addrString)
+
+	// create peer.Peer instance
+	p, err := peer.NewOutboundPeer(peerCfg, a)
 	if err != nil {
 		fmt.Printf("NewOutboundPeer: error %v\n", err)
-		offline = append(offline, p)
-		//panic("outbound error")
+		return
 	}
 
-	// Establish the connection to the peer address and mark it connected.
+	// give peer.Peer instance a tcp connection
 	conn, err := net.Dial("tcp", p.Addr())
 	if err != nil {
 		fmt.Printf("net.Dial: error %v\n", err)
 		offline = append(offline, p)
-		//panic("net.Dial error")
 		return
 	}
 	p.AssociateConnection(conn)
@@ -87,7 +87,6 @@ func handshake(addr wire.NetAddress) {
 	// wait for completion or timeout
 	select {
 	case <-finished:
-		// do nothing
 	case <-time.After(time.Second * 3):
 		offline = append(offline, p)
 	}
@@ -96,9 +95,12 @@ func handshake(addr wire.NetAddress) {
 }
 
 func worker() {
+	counter := 1
 	for true {
-		addr := <-inbox
-		handshake(*addr)
+		addr := inbox.Pop()
+		visit(addr.(string))
+		fmt.Println(counter)
+		counter++
 	}
 }
 
@@ -106,7 +108,7 @@ func collect() {
 	for true {
 		// print a report
 		fmt.Printf("online %d, offline %d, inbox %d\n",
-			len(online), len(offline), len(inbox))
+			len(online), len(offline), inbox.Length())
 
 		// sleep until next loop
 		time.Sleep(1 * time.Second)
@@ -115,7 +117,7 @@ func collect() {
 
 func main() {
 	// launch workers
-	for i := 1; i < 1000; i++ {
+	for i := 1; i < 500; i++ {
 		go worker()
 	}
 
