a0af637091d4501e96accb802102b40aaa63bd25
Justin Moon <mail@justinmoon.com>
2019-05-27 00:07:29 -0500

    remove queue module, socket timeouts

--- main.go
+++ main.go
@@ -9,16 +9,18 @@ import (
 	"github.com/btcsuite/btcd/connmgr"
 	"github.com/btcsuite/btcd/peer"
 	"github.com/btcsuite/btcd/wire"
-	"github.com/sheerun/queue"
+	"github.com/deckarep/golang-set"
 )
 
 // globals
 
 // q contains addresses we need to visit
-//var inbox = make(chan *wire.NetAddress)
-var inbox = queue.New()
-var online = make([]*peer.Peer, 2)
-var offline = make([]*peer.Peer, 2)
+var inbox = make(chan *wire.NetAddress, 10000000)
+
+//var inbox = queue.New()
+//var online = make([]*peer.Peer, 2)
+var online = mapset.NewSet()
+var offline = mapset.NewSet()
 
 // HandleVerack prints when we've received a verack message
 
@@ -30,7 +32,7 @@ func queryDNS() {
 	connmgr.SeedFromDNS(&params, defaultRequiredServices,
 		net.LookupIP, func(addrs []*wire.NetAddress) {
 			for _, addr := range addrs {
-				inbox.Append(NAToS(addr))
+				inbox <- addr
 			}
 		})
 	<-time.After(time.Second * 10) // HACK!!!
@@ -40,8 +42,9 @@ func NAToS(na *wire.NetAddress) string {
 	return fmt.Sprintf("[%s]:%d", na.IP, na.Port)
 }
 
-func visit(a string) {
+func visit(addr *wire.NetAddress) {
 	var finished = make(chan bool)
+	var handshakeSuccessful = false
 	peerCfg := &peer.Config{
 		UserAgentName:    "mooniversity",
 		UserAgentVersion: "0.0.1",
@@ -54,70 +57,93 @@ func visit(a string) {
 				getaddrSent := make(chan struct{}) // TODO: delete
 				p.QueueMessage(getaddr, getaddrSent)
 				<-getaddrSent
-				online = append(online, p)
+				online.Add(p.NA().IP.String())
+				handshakeSuccessful = true
 			},
 			OnAddr: func(p *peer.Peer, msg *wire.MsgAddr) {
 				if len(msg.AddrList) > 1 {
-					fmt.Printf("received %d addr from %s\n", len(msg.AddrList), a)
+					//fmt.Printf("received %d addr from %s\n", len(msg.AddrList), NAToS(addr))
 					for _, addr := range msg.AddrList {
-						inbox.Append(NAToS(addr))
+						inbox <- addr
 					}
+					finished <- true
 				}
-				finished <- true
 			},
 		},
 	}
 
 	// create peer.Peer instance
-	p, err := peer.NewOutboundPeer(peerCfg, a)
+	p, err := peer.NewOutboundPeer(peerCfg, NAToS(addr))
 	if err != nil {
-		fmt.Printf("NewOutboundPeer: error %v\n", err)
+		fmt.Printf("NewOutboundPeer: %v\n", err)
 		return
 	}
 
 	// give peer.Peer instance a tcp connection
-	conn, err := net.Dial("tcp", p.Addr())
+	d := net.Dialer{Timeout: time.Second}
+	conn, err := d.Dial("tcp", p.Addr())
 	if err != nil {
-		fmt.Printf("net.Dial: error %v\n", err)
-		offline = append(offline, p)
+		fmt.Printf("net.Dial: %v\n", err)
+		offline.Add(p.NA().IP.String())
 		return
 	}
 	p.AssociateConnection(conn)
 
 	// wait for completion or timeout
+	//fmt.Println("connected")
 	select {
 	case <-finished:
 	case <-time.After(time.Second * 3):
-		offline = append(offline, p)
+		if !handshakeSuccessful {
+			fmt.Println("timeout")
+			offline.Add(p.NA().IP.String())
+		}
 	}
+
 	p.Disconnect()
 	p.WaitForDisconnect()
+	//fmt.Println("disconnected\n")
 }
 
 func worker() {
-	counter := 1
 	for true {
-		addr := inbox.Pop()
-		visit(addr.(string))
-		fmt.Println(counter)
-		counter++
+		addr := <-inbox
+		for {
+			isNew := !(online.Contains(addr.IP.String()) ||
+				online.Contains(addr.IP.String()))
+			if isNew {
+				break
+			}
+			addr = <-inbox
+		}
+		visit(addr)
 	}
 }
 
 func collect() {
+	nOnline := 0
+	incOnline := 0
+	nOffline := 0
+	incOffline := 0
 	for true {
+		offlineSlice := offline.ToSlice()
+		onlineSlice := online.ToSlice()
+		incOnline = len(onlineSlice) - nOnline
+		incOffline = len(offlineSlice) - nOffline
 		// print a report
-		fmt.Printf("online %d, offline %d, inbox %d\n",
-			len(online), len(offline), inbox.Length())
+		fmt.Printf("online %d (%d), offline %d (%d), inbox %d\n",
+			len(onlineSlice), incOnline, len(offlineSlice), incOffline, len(inbox))
+		nOnline = len(onlineSlice)
+		nOffline = len(offlineSlice)
 
 		// sleep until next loop
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 }
 
 func main() {
 	// launch workers
-	for i := 1; i < 500; i++ {
+	for i := 1; i < 1000; i++ {
 		go worker()
 	}
 
